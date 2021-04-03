ca38cf2a68bd261fec5b91d4b98b36e5b34db260
bcooperstl <coop999@gmail.com>
2021-04-03 14:15:24 -0500

    Added day 4 part 2 solution

--- src/solutions/aoc_day_4.cpp
+++ src/solutions/aoc_day_4.cpp
@@ -64,3 +64,36 @@ string AocDay4::part1(string filename, vector<string> extra_args)
     out << i;
     return out.str();
 }
+
+string AocDay4::part2(string filename, vector<string> extra_args)
+{
+    string data_str = read_input(filename);
+    unsigned char result[MD5_DIGEST_LENGTH];
+    int i = 0;
+    char input[32];
+    bool found = false;
+    char data[32];
+    
+    strncpy(data, data_str.c_str(), 32);
+
+    cout << "Checking for input " << data << endl;
+    while (!found)
+    {
+        i++;
+        if (i % 100000 == 0)
+        {
+            cout << " On iteration " << i << endl;
+        }
+        int length = snprintf(input, 32, "%s%d", data, i);
+        MD5((unsigned char *)input, length, result);
+        if ((result[0] == 0) && (result [1]) == 0 && (result[2] == 0))
+        {
+            cout << " Found matching hash target at i=" << i << endl;
+            found = true;
+        }
+    }
+    
+    ostringstream out;
+    out << i;
+    return out.str();
+}
