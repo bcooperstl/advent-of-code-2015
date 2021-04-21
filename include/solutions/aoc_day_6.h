3832d3adda9c4937a1d5475ea5908e0a3101b80a
bcooperstl <coop999@gmail.com>
2021-04-21 14:07:36 -0500

    Created namespace for Day6 to try to make things look cleaner as proof of concept for Day7

--- include/solutions/aoc_day_6.h
+++ include/solutions/aoc_day_6.h
@@ -7,50 +7,54 @@
 #define FIELD_SIZE_Y 1000
 
 
-enum Day6Operation
+namespace Day6
 {
-    TurnOn,
-    TurnOff,
-    Toggle
-};
+    enum Operation
+    {
+        TurnOn,
+        TurnOff,
+        Toggle
+    };
+    
+    
+    struct Instruction
+    {
+        Operation operation;
+        int min_x;
+        int max_x;
+        int min_y;
+        int max_y;
+    };
+    
+    class Field
+    {
+        private:
+            bool m_field[FIELD_SIZE_Y][FIELD_SIZE_X]; // y index first, then x index
+        public:
+            Field();
+            ~Field();
+            int count_elements(bool status);
+            void process_instruction(Instruction instruction);    
+    };
+    
+    class BrightnessField
+    {
+        private:
+            int m_field[FIELD_SIZE_Y][FIELD_SIZE_X]; // y index first, then x index
+        public:
+            BrightnessField();
+            ~BrightnessField();
+            long sum_brightness();
+            void process_instruction(Instruction instruction);    
+    };
+}
 
-
-struct Day6Instruction
-{
-    Day6Operation operation;
-    int min_x;
-    int max_x;
-    int min_y;
-    int max_y;
-};
-
-class Day6Field
-{
-    private:
-        bool m_field[FIELD_SIZE_Y][FIELD_SIZE_X]; // y index first, then x index
-    public:
-        Day6Field();
-        ~Day6Field();
-        int count_elements(bool status);
-        void process_instruction(Day6Instruction instruction);    
-};
-
-class Day6BrightnessField
-{
-    private:
-        int m_field[FIELD_SIZE_Y][FIELD_SIZE_X]; // y index first, then x index
-    public:
-        Day6BrightnessField();
-        ~Day6BrightnessField();
-        long sum_brightness();
-        void process_instruction(Day6Instruction instruction);    
-};
-
+using namespace Day6;
 
 class AocDay6 : public AocDay
 {
     private:
-        vector<Day6Instruction> parse_input(string filename);
+        vector<Instruction> parse_input(string filename);
     public:
         AocDay6();
         ~AocDay6();
