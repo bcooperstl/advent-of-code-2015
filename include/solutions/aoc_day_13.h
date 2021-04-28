0b5f11ba7143e1c13af7fe857d752e6ca40a6ccd
bcooperstl <coop999@gmail.com>
2021-04-28 15:05:15 -0500

    Work in progress for day 13 part 1. Test case runs. Has my modified implementation of heaps algorithm

---
+++ include/solutions/aoc_day_13.h
@@ -0,0 +1,39 @@
+#ifndef __AOC_DAY_13__
+#define __AOC_DAY_13__
+
+#include <map>
+
+#include "aoc_day.h"
+
+#define MAX_PEOPLE 10
+
+namespace Day13
+{
+    struct Person
+    {
+        string name;
+        map<Person *, int> units;
+    };
+}
+
+using namespace Day13;
+
+class AocDay13 : public AocDay
+{
+    private:
+        int parse_input(string filename, Person ** people); // returns the number of cities created
+        int calculate_happiness(Person ** people, int num_people);
+        void modified_heaps_algorithm_generate(Person ** people, int num_people, int k, int & best_case);
+        int find_best_arrangement(Person ** people, int num_people);
+        void dump_cycle(Person ** people, int num_people);
+    public:
+        AocDay13();
+        ~AocDay13();
+        string part1(string filename, vector<string> extra_args);
+        //string part2(string filename, vector<string> extra_args);
+        static int compare_people_by_name_func(const void *left, const void *right);
+        static int compare_people_by_name(Person *left, Person *right);
+};
+
+
+#endif
